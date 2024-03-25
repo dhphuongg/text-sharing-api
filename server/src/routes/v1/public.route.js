e928b347b98c9839b3d7eeaaa43ca05bd060e7fd
phuonghight <hoangphuong270703@gmail.com>
2024-03-25 21:33:41 +0700

    [server] feat: make get post by id to public api

--- server/src/routes/v1/public.route.js
+++ server/src/routes/v1/public.route.js
@@ -1,13 +1,11 @@
 const router = require("express").Router();
 
 const { validate } = require("../../middlewares");
-const { userValidation } = require("../../validations");
-const { userController } = require("../../controllers");
+const { userValidation, postValidation } = require("../../validations");
+const { userController, postController } = require("../../controllers");
 
-router.get(
-  "/user/:id",
-  validate(userValidation.getById),
-  userController.getById
-);
+router
+  .get("/user/:id", validate(userValidation.getById), userController.getById)
+  .get("/post/:id", validate(postValidation.getById), postController.getById);
 
 module.exports = router;
